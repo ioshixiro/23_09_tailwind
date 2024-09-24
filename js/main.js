c00ab041190b82fb6a5610d61afc7ed887ccd38a
Ioshixiro <lazizuchiha@gmail.com>
2024-09-24 21:23:51 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// let str = 'camelCase'
+
+// let str2 = " ";
+
+// for(let i = 0 ; i < str.length ; i++){
+//     if(str[i] === str[i].toUpperCase()){
+//         str2 += " " + str[i]
+       
+//     }else  str2 += str[i];
+// }
+
+// console.log(str2);
+
+
+// let str = 'camelCase';
+// let str2 = " ";
+
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] === str[i].toUpperCase()) {
+//         str2 += " " + str[i];
+//     } else {
+//         str2 += str[i];
+//     }
+// }
+
+// console.log(str2); // camel Case
+
+// 1-masala
+
+// function sc(floor) {
+//   let result = "";
+  
+//   for (let i = 1; i <= floor; i++) {
+//     if (i <= 6) {
+//       result += "Aa! ";  
+//     } else if( i > 6) {
+//       result += "Pa! ";  
+//     }else
+//     result =+ " ";
+//   }
+
+//   return result.trim();  
+// }
+
+// console.log(sc(2));  
+// console.log(sc(7));  
+
+// 2-masala
+
+// function InBinary(n) {
+//     let count = 0;
+
+//     while (n > 0 && n % 2 === 0) {
+//         count++;
+//         n = Math.floor(n / 2);
+//     }
+
+//     return count;
+// }
+
+
+
+// console.log(InBinary(4)); 
+
+// 3-masala
+
+
+// function dragon(oq,ajdar){
+//     return oq >= ajdar * 2;
+// }
+
+
+
+// console.log(dragon(10,5));
+// console.log(dragon(7,4));
+// console.log(dragon(4,2));
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+function dragon(ajdar,oq){
+    return oq >= ajdar * 2;
+}
+
+
+
+console.log(dragon(10,5));
+console.log(dragon(7,4));
+console.log(dragon(4,2));
